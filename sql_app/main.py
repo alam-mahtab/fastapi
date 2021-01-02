daa90f51ee8b7d32542148655253bbda23970efd
alam-mahtab <67095487+alam-mahtab@users.noreply.github.com>
2021-01-02 16:50:09 +0530

    little change after get login

--- sql_app/main.py
+++ sql_app/main.py
@@ -108,8 +108,8 @@ def create_user(
     # , status_code=status.HTTP_303_SEE_OTHER)
     #return RedirectResponse(url="/users",status_code=status)
 @app.get("/users/")
-async def main(current_user: models.User = Depends(get_current_user)):
-    return {"message": "Hello " + current_user + ""}
+def get_user(username: str, password: str):
+    return {"message": "Hello " + username + ""}
 
 @app.post("/login/")
 def login(db: Session = Depends(get_db), form_data:OAuth2PasswordRequestForm = Depends()):
@@ -122,8 +122,9 @@ def login(db: Session = Depends(get_db), form_data:OAuth2PasswordRequestForm = D
         )
     return {"access_token": user.username, "token_type": "bearer"}
 @app.get("/login/")
-def test():
-    return {"message": "Hello " }
+def get_user(db, username: str):
+    
+    return {"message": "Hello "+ username + "" }
 #if user.is_authenticated:   
 #@app.post("/logout/")
 
